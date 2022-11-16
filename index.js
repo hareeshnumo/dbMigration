10d961cc9d2f72ad709d52f8306ca87646aed3c6
hareeshnumo <hareesh.km@numocity.com>
2022-11-16 16:22:40 +0530

    changes

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const greet=(name)=>{
-    console.log(`hi ${name}`);
+const greet=(name, env)=>{
+    console.log(`hi ${name} from ${environment}`);
 }
+
+
+var arguments = process.argv
+const environment=arguments[2];
+const nameData=arguments[3];
+greet(nameData, environment)
