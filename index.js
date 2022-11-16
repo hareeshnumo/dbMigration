6d0a96a7609a75113844fea48c75081017cdb58d
hareeshnumo <hareesh.km@numocity.com>
2022-11-16 17:18:45 +0530

    input params changes

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const greet=(name, env)=>{
-    console.log(`hi ${name} from ${environment}`);
-}
+const greet=(MongoURI, MYSQL_USER_HOST, MYSQL_PASSWORD, MYSQL_BILLING_HOST, MYSQL_NOTIFICATION_HOST)=>{
+    console.log(`MongoURI ${MongoURI},
+     MYSQL_USER_HOST ${MYSQL_USER_HOST}, MYSQL_PASSWORD ${MYSQL_PASSWORD}, 
+     MYSQL_BILLING_HOST ${MYSQL_BILLING_HOST}, MYSQL_NOTIFICATION_HOST ${MYSQL_NOTIFICATION_HOST}`);
+};
 
 
 var arguments = process.argv
-const environment=arguments[2];
-const nameData=arguments[3];
-greet(nameData, environment)
+const MongoURI=arguments[2];
+const MYSQL_USER_HOST=arguments[3];
+const MYSQL_PASSWORD= arguments[4];
+const MYSQL_BILLING_HOST= arguments[5];
+const MYSQL_NOTIFICATION_HOST= arguments[6];
+
+greet(MongoURI, MYSQL_USER_HOST, MYSQL_PASSWORD, MYSQL_BILLING_HOST, MYSQL_NOTIFICATION_HOST);
